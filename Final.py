77b2ba9eed18a34b80959bf6ffb59d81256ba7f1
KyleSandersI <91567348+KyleSandersI@users.noreply.github.com>
2021-12-06 11:25:40 -0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -159,6 +159,7 @@ st.caption("Estimating a players possible production across a season can be diff
 sc_plot = alt.Chart(df).mark_point().encode(
     y = "G/A",
     x ="Mins",
+    color = "rainbow",
     tooltip = ["G/A","Age","Name","Club"]
 ).properties (
     width = 800,
